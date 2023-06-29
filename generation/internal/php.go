e0a21f3e056366fd87c6fa183389e0fa1ff7b16c
Shaun Kaasten <shaun@circleci.com>
2023-06-29 15:37:53 -0400

    Infer the php image from the composer.json when present
    CVL-552

--- generation/internal/php.go
+++ generation/internal/php.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"strings"
+
 	"github.com/CircleCI-Public/circleci-config/config"
 	"github.com/CircleCI-Public/circleci-config/labeling/labels"
 )
@@ -34,6 +36,16 @@ func initialPhpSteps(ls labels.LabelSet) []config.Step {
 	return []config.Step{checkout, installPackages}
 }
 
+func phpImageVersion(composerVersion string) string {
+	version := "8.2.7"
+	if composerVersion != "" {
+		version = strings.TrimPrefix(composerVersion, "~")
+		version = strings.TrimPrefix(version, "^")
+		version = strings.TrimSuffix(version, ".*")
+	}
+	return "cimg/php:" + version + "-node"
+}
+
 func phpTestJob(ls labels.LabelSet) *Job {
 	steps := initialPhpSteps(ls)
 	steps = append(steps, config.Step{
@@ -46,7 +58,7 @@ func phpTestJob(ls labels.LabelSet) *Job {
 			Name:         "test-php",
 			Comment:      "Install php packages and run tests",
 			Steps:        steps,
-			DockerImages: []string{"cimg/php:8.2.7-node"},
+			DockerImages: []string{phpImageVersion(ls[labels.DepsPhp].Dependencies["php"])},
 		},
 		Orbs: map[string]string{
 			"php": "circleci/php@1",
